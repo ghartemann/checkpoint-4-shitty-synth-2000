e31f1b4d9798feb9b0386608684d5571f36c2723
ghartemann <g.hartemann@gmail.com>
2022-07-20 10:05:26 +0200

    created logout route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -23,4 +23,9 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+    }
 }
