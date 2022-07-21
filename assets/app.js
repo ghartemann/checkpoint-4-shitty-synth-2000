830306fbd031a00b7803e7a84c07f4d074e501bf
ghartemann <g.hartemann@gmail.com>
2022-07-21 11:37:25 +0200

    added edit and a few other things too

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,8 @@ import 'animate.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
