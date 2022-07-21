708ac92ecd0c5e6dee7a1ad94286b779d8936803
ghartemann <g.hartemann@gmail.com>
2022-07-21 20:19:50 +0200

    many things were done

--- src/DataFixtures/TrackFixtures.php
+++ src/DataFixtures/TrackFixtures.php
@@ -58,18 +58,45 @@ class TrackFixtures extends Fixture implements DependentFixtureInterface
         [
             'title' => 'Happy Birthday to You',
             'notes' => 'C C D C F E C C D C G F C C C5 A F E D Bb Bb A F G F',
-            'letters' => 'Q Q S Q F D Q Q S Q G F Q Q K H F D S U U H F G F ',
+            'letters' => 'Q Q S Q F D Q Q S Q G F Q Q K H F D S U U H F G F',
             'artist' => null,
             'picture' => 'https://cdns-images.dzcdn.net/images/cover/e620bddf88338269999ff1b8b0381c4a/350x350.jpg',
             'difficulty' => 3,
             'youtube' => null,
         ],
+        [
+            'title' => 'Running Up That Hill',
+            'notes' => 'Eb Bb Bb Bb Bb G Eb Bb Bb Bb Bb D C Bb Bb Bb Bb G Bb C Bb Bb Bb Bb G G F Eb F Eb',
+            'letters' => 'E U U U U G E U U U U S Q U U U U G U Q U U U U G G F E F E',
+            'artist' => 'Kate Bush',
+            'picture' => 'https://m.media-amazon.com/images/I/71QN-MHfywL._SX355_.jpg',
+            'difficulty' => 3,
+            'youtube' => "wp43OdtAAkM",
+        ],
+        [
+            'title' => 'Never Gonna Give You Up',
+            'notes' => 'C D F D A A G C D F D G G F E D C D F D F G E D C C G F',
+            'letters' => 'Q S F S H H G Q S F S G G F D S Q S F S F G D S Q Q G F',
+            'artist' => 'Rick Astley',
+            'picture' => 'https://fr.shopping.rakuten.com/photo/975883067.jpg',
+            'difficulty' => 2,
+            'youtube' => 'dQw4w9WgXcQ',
+        ],
+        [
+            'title' => 'Don\'t Stop Me Now',
+            'notes' => 'E F G C5 E E D D C D E D E E G E',
+            'letters' => 'D F G K D D S S Q S D S D D G D',
+            'artist' => 'Queen',
+            'picture' => 'https://cdns-images.dzcdn.net/images/cover/9d5f6e56b755d104525cf3a0810356bc/500x500.jpg',
+            'difficulty' => 2,
+            'youtube' => 'HgzGwKwLmgM',
+        ],
+
+
     ];
 
     public function load(ObjectManager $manager): void
     {
-        $i = 0;
-
         foreach (self::TRACKS as $trackName) {
             $note = new Track();
             $note
