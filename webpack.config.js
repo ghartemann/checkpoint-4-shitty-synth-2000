27772053eb8055b209e254af9d99ed2a55a544a9
ghartemann <g.hartemann@gmail.com>
2022-07-22 14:29:23 +0200

    fixed js being in the wrong place

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('synth', './assets/scripts/synth.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
